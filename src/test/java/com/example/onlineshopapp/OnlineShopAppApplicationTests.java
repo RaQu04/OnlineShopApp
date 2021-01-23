ba4504f3871ebb19410c3c27d5de9da23f6a5ea6
lukaszRakowiecki <lukasz.er17@gmail.com>
2021-01-23 12:13:57 +0100

    first commit, created branch dev

---
+++ src/test/java/com/example/onlineshopapp/OnlineShopAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineshopapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
