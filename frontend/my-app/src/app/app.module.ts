f550e235c24302922f9d466bbc77ac86545c8ed1
lukaszRakowiecki <lukasz.er17@gmail.com>
2021-01-24 18:52:34 +0100

    created login form

--- frontend/my-app/src/app/app.module.ts
+++ frontend/my-app/src/app/app.module.ts
@@ -1,28 +1,32 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
-import {RouterModule, Routes} from '@angular/router';
-import {MatInputModule} from '@angular/material/input';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule, Routes } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
 
-import {AppComponent} from './app.component';
-import {NavComponent} from './nav/nav.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HomeComponent} from './home/home.component';
-import {CategoryComponent} from './category/category.component';
-import {AboutComponent} from './about/about.component';
-import {ContactComponent} from './contact/contact.component';
-import {BasketComponent} from './basket/basket.component';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { CategoryComponent } from './category/category.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BasketComponent } from './basket/basket.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { LoginPanelComponent } from './login-panel/login-panel.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 const routes: Routes = [
-  {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: HomeComponent},
-  {path: 'category', component: CategoryComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'contact', component: ContactComponent},
-  {path: 'basket', component: BasketComponent}];
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'category', component: CategoryComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'basket', component: BasketComponent },
+  { path: 'login', component: LoginPanelComponent }];
 
 @NgModule({
   declarations: [
@@ -32,7 +36,8 @@ const routes: Routes = [
     CategoryComponent,
     AboutComponent,
     ContactComponent,
-    BasketComponent
+    BasketComponent,
+    LoginPanelComponent
   ],
   imports: [
     BrowserModule,
@@ -41,11 +46,16 @@ const routes: Routes = [
     MatButtonModule,
     MatIconModule,
     MatInputModule,
+    MatCardModule,
+    FormsModule,
     RouterModule.forRoot(routes),
-    NgbModule
+    NgbModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {
 }
+
+
