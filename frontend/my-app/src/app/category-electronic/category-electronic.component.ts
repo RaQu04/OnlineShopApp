35f9310df69f7a51ec9624b41cf8609dc5f8e17d
Lukasz Rakowiecki <lukasz.er17@gmail.com>
2021-02-01 18:44:51 +0100

    modify category-electronic.component.ts

--- frontend/my-app/src/app/category-electronic/category-electronic.component.ts
+++ frontend/my-app/src/app/category-electronic/category-electronic.component.ts
@@ -13,8 +13,5 @@ export class CategoryElectronicComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  public getDescriptionDell(): string {
-    return '<div> <p>Ekran 15.6", 1920 x 1080px, 60Hz</p> <p>Procesor Intel Core i5-10300H</p> <p>Wielkość pamięci RAM[GB] 8</p>  <p>Dysk 512 GB SSD</p>  <p>Karta graficzna NVIDIA GeForce GTX 1650 TI</p> <p>System operacyjny Windows 10 Home</p></div>';
-  }
 
 }
